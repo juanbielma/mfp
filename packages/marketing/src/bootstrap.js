2d9c766ab83301289b190a04abf40875a500a8b1
juanbielma <juan.bielma@globant.com>
2023-09-22 19:26:21 -0600

    Initial commit

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+function mount(el) {
+  ReactDOM.render(<App />, el);
+}
+
+// if we are in development and in isolation call mount immediately
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_marketing-dev-root");
+
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
+
+//export mount function
+export { mount };
