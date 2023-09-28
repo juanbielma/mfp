b01416ae43286e5dbc480297e65327d8bacb6406
juanbielma <juan.bielma@globant.com>
2023-09-28 15:12:30 -0600

    Added Dashboard

--- packages/dashboard/config/webpack.dev.js
+++ packages/dashboard/config/webpack.dev.js
@@ -1,5 +1,6 @@
 const { merge } = require("webpack-merge");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
@@ -7,23 +8,28 @@ const packageJson = require("../package.json");
 const devConfig = {
   mode: "development",
   devServer: {
-    port: 8080,
+    port: 8083,
     historyApiFallback: {
       index: "/index.html",
     },
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
   },
   output: {
-    publicPath: "http://localhost:8080/",
+    publicPath: "http://localhost:8083/",
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "container",
-      remotes: {
-        marketing: "marketing@http://localhost:8081/remoteEntry.js",
-        auth: "auth@http://localhost:8082/remoteEntry.js",
-        dashboard: "dashboard@http://localhost:8083/remoteEntry.js",
+      name: "dashboard",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./DashboardApp": "./src/bootstrap",
       },
       shared: packageJson.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
     }),
   ],
 };
