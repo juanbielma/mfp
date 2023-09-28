f6e630123b7b2d81d4beb0f9179a8fa89c2d05d4
juanbielma <juan.bielma@globant.com>
2023-09-28 11:01:59 -0600

    Added auth subapp

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App";
 
-function mount(el, { onNavigate, defualtHistory, initialPath }) {
+function mount(el, { onNavigate, defualtHistory, initialPath, onSignIn }) {
   const history =
     defualtHistory ||
     createMemoryHistory({
@@ -12,7 +12,7 @@ function mount(el, { onNavigate, defualtHistory, initialPath }) {
 
   if (onNavigate) history.listen(onNavigate);
 
-  ReactDOM.render(<App history={history} />, el);
+  ReactDOM.render(<App history={history} onSignIn={onSignIn} />, el);
 
   return {
     onParentNavigate({ pathname: nextPathname }) {
@@ -24,7 +24,7 @@ function mount(el, { onNavigate, defualtHistory, initialPath }) {
 
 // if we are in development and in isolation call mount immediately
 if (process.env.NODE_ENV === "development") {
-  const devRoot = document.querySelector("#_marketing-dev-root");
+  const devRoot = document.querySelector("#_auth-dev-root");
 
   if (devRoot) {
     mount(devRoot, { defualtHistory: createBrowserHistory() });
